1b286f351dba8bb53e7a832f8aba67fc31858721
momonomono <momonomono1128@gmail.com>
2021-07-09 13:52:32 +0900

    loginのhtmlを書きました

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -1,9 +1,26 @@
 <template>
-    <div class="">
-        <h2>Login</h2>
-        <label class="p-label__form">
+    <div class="l-form-container">
+        <section class="p-form">
+            <h2>Login</h2>
 
-        </label>
+
+            <label class="p-form__label">
+                <p class=""></p>
+                <input type="text" class="">
+            </label>
+            
+            <label class="p-form__label">
+                <p class=""></p>
+                <input type="text" class="">
+            </label>
+
+            <label class="p-form__label">
+                <input type="checkbox" name="" id="">
+                <p></p>
+            </label>
+
+            <button></button>
+        </section>
     </div>
 </template>
 
